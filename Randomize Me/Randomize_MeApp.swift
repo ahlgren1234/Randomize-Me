66658c7d60c8a24f0b761ce742ebd8eca85bb40c
Peter Ahlgren <ahlgren1234@gmail.com>
2022-02-23 15:36:42 +0100

    Initial Commit

---
+++ Randomize Me/Randomize_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Randomize_MeApp.swift
+//  Randomize Me
+//
+//  Created by Peter Ahlgren on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct Randomize_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
