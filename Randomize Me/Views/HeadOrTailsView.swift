4e403668056a5cf765ab93bc67787d7f241f2bd8
Peter Ahlgren <ahlgren1234@gmail.com>
2022-03-27 09:54:15 +0200

    app icons, launch screen and head or tails.

--- Randomize Me/Views/HeadOrTailsView.swift
+++ Randomize Me/Views/HeadOrTailsView.swift
@@ -8,8 +8,82 @@
 import SwiftUI
 
 struct HeadOrTailsView: View {
+    
+    struct Unique: Identifiable, ExpressibleByStringLiteral {
+        let value: String
+        let id = UUID()
+        
+        init(stringLiteral value: String) {
+            self.value = value
+        }
+    }
+    
+    @State var answerList: [Unique] = []
+    
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 170))
+    ]
+    
     var body: some View {
-        Text("Head Or Tails View")
+        
+        VStack {
+            Text("Head or Tails")
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .foregroundColor(.accentColor)
+            
+            Divider()
+            
+            ScrollView {
+                
+                LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                    
+                    ForEach(answerList, id: \.id) { item in
+
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 170, height: 80)
+                                .foregroundColor(.green)
+                                .cornerRadius(20)
+
+                            Text("\(item.value)")
+                                .foregroundColor(.white)
+                                .font(.system(size: 30, weight: .medium, design: .rounded))
+
+                        } //: ZSTACK
+                    }
+                    
+                } //: LAZYVGRID
+                
+            } //: SCROLLVIEW
+            
+            VStack {
+                Divider()
+                
+                Button {
+                    answerList = []
+                } label: {
+                    Text("Clear")
+                }
+                
+                Button {
+                    let answers = ["Head", "Tails"]
+                    let randomAnswer = answers.randomElement()
+                    let newAnswer = Unique(stringLiteral: randomAnswer!)
+                    answerList.append(newAnswer)
+                    
+                } label: {
+                    Text("Randomize")
+                }
+                .foregroundColor(Color.white)
+                .padding()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(8)
+                .padding()
+                
+            } //:VSTACK
+            
+        } //: VSTACK
     }
 }
 
